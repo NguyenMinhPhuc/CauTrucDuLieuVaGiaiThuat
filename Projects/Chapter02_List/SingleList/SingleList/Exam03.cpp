9fd0750b81138bfb69a5ac64dd92c8e5ea73dba1
Nguyen Minh Phuc <nmphuc@outlook.com>
2019-08-20 14:12:15 +0700

    Hoàn tất bài tập 3 thêm phần ghi chú

--- Projects/Chapter02_List/SingleList/SingleList/Exam03.cpp
+++ Projects/Chapter02_List/SingleList/SingleList/Exam03.cpp
@@ -1,26 +1,33 @@
+﻿//cho da thức P(x)=anXn+an-1Xn-1+...+a1X+a0
+//Hãy định nghĩa và xây dựng DSLK đơn lưu trữ đa thức P(x). 
+//1. cài đặt cấu trúc dữ liệu
+//2. Nhập đa thức
+//3. In đa thức.
+//4. Tính đạo hàm của đa thức
 #include "iostream"
 
 using namespace std;
-
+//Định nghĩa kiểu dữ liệu Da Thức
 typedef struct dathuc
 {
 	int heSo;
 	int luyThua;
 }DaThuc;
-
+//Định nghĩa kiểu node cho danh sách
 typedef struct node
 {
 	DaThuc data;
 	node* link;
 }NODE;
+//Định nghĩa kiểu con trỏ NODE
 typedef NODE* NodeType;
-
+//Định nghĩa kiểu danh sách để lưu trữ đa thức
 typedef struct list
 {
 	NodeType first;
 	NodeType last;
 }LIST;
-
+//Phương thức tạo NODE có giá trị da thức x
 NodeType GetNode(DaThuc x)
 {
 	NodeType p =(NodeType) malloc(sizeof(NODE));
@@ -32,11 +39,13 @@ NodeType GetNode(DaThuc x)
 	}
 	return p;
 }
+//Phương thức tạo danh sách rỗng
 void InitList(LIST &list)
 {
 	list.first = NULL;
 	list.last = NULL;
 }
+//Phương thức thêm 1 phần tử vào đầu danh sách
 void AddFirst(LIST&list,NodeType new_ele)
 {
 	if (list.first == NULL)
@@ -50,6 +59,7 @@ void AddFirst(LIST&list,NodeType new_ele)
 		list.first = new_ele;
 	}
 }
+//Phương thức thêm 1 phần tử vào đuôi danh sách phương thức nay được dùng.
 void AddLast(LIST& list, NodeType new_ele)
 {
 	if (list.first == NULL)
@@ -63,14 +73,14 @@ void AddLast(LIST& list, NodeType new_ele)
 		list.last = new_ele;
 	}
 }
-
+//Thêm đa thức x vào list
 void InsertList(LIST& list, DaThuc x)
 {
 	NodeType p = GetNode(x);
 	if (p != NULL)
 		AddLast(list, p);
 }
-
+//Nhập đa thức
 void InputDaThuc(LIST& list)
 {
 	DaThuc dt;
@@ -86,6 +96,7 @@ void InputDaThuc(LIST& list)
 		InsertList(list, dt);
 	}
 }
+//in giá trị một Node
 void PrintResult(LIST list,NodeType p)
 {
 	if (p != list.first && p->data.heSo >= 0)
@@ -96,6 +107,7 @@ void PrintResult(LIST list,NodeType p)
 	else if (p->data.luyThua == 1)
 		cout << "x ";
 }
+//Phương thức in đa thức ra màn hình
 void PrintDaThuc(LIST list)
 {
 	NodeType p = list.first;
@@ -106,7 +118,7 @@ void PrintDaThuc(LIST list)
 	}
 	cout << endl;
 }
-
+//Phương thức tính đạo hàm
 void TinhDaoHam(LIST list)
 {
 	NodeType p = list.first;
@@ -124,6 +136,7 @@ void TinhDaoHam(LIST list)
 	}
 	PrintDaThuc(listnew);
 }
+//Phương thức hàm main.
 void main()
 {
 	LIST list;
